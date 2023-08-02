e87971dbe36cf0eb7ced82470a65f56a95d90a5f
muhanisss <muhammedmuhnis.cse21@jecc.ac.in>
2023-08-02 23:39:53 +0530

    Search Function Added

--- Binary Search.c
+++ Binary Search.c
@@ -48,3 +48,34 @@ void sort(int a[],int n)
 		printf("%d ",a[j]);
 	}
 }
+int search(int a[],int n,int x)
+{
+	int i=0,l=n-1,mid;
+	while(i<=l)
+	{
+		mid=(i+l)/2;
+		if(x==a[mid])
+		{
+			i=-1;
+			break;
+		}
+		else if(x<a[mid])
+		{
+			l=mid-1;
+		}
+		else
+		{
+			i=mid+1;
+			
+		}
+	}
+	if(i==-1)
+	{
+		printf("Element is found at Location %d th location of the array",mid+1);
+	}
+	else
+	{
+		printf("Element is not found in the array");
+	}
+	return 0;
+}
