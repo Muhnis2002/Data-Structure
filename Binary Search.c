4fa1379526ad74b47e810fd695e3eaf620fc71f8
muhanisss <muhammedmuhnis.cse21@jecc.ac.in>
2023-08-02 23:39:16 +0530

    Read and Sort Function Added

--- Binary Search.c
+++ Binary Search.c
@@ -18,3 +18,33 @@ int main()
 	search(a,n,x);
 	return 0;
 }
+int read(int a[],int n)
+{
+	for(int i=0;i<n;++i)
+	{
+		printf("Enter the %d th Element of the array : ",i+1);
+		scanf("%d",&a[i]);
+	}
+	return 0;
+}
+void sort(int a[],int n)
+{
+	int t;
+	for(int i=0;i<n-1;++i)
+	{
+		for(int j=0;j<n-i-1;++j)
+		{
+			if(a[j+1]<a[j])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+	printf("The Sorted array for performing Binary Search is : ");
+	for(int j=0;j<n;++j)
+	{
+		printf("%d ",a[j]);
+	}
+}
