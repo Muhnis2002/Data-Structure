feccb95cc838c5b435145c5945ce995dde8beaf2
muhanisss <muhammedmuhnis.cse21@jecc.ac.in>
2023-08-03 09:52:17 +0530

    Main Function Completed and all other Required Functions Defined

---
+++ Merge Sort.c
@@ -0,0 +1,21 @@
+ /*Muhammed Muhnis T K
+  Sorting an array using Merge Sort Technique
+  JEC21CS083*/
+#include<stdio.h>
+int read(int a[],int l);
+int print(int a[],int l);
+int mergesort(int a[],int f,int l);
+int merge(int a[],int f,int m,int l);
+int main()
+{
+	int a[25],n;
+	printf("Enter the limit of array : ");
+	scanf("%d",&n);
+	read(a,n);
+	printf("The Unsorted array is : ");
+	print(a,n);
+	mergesort(a,0,n-1);
+	printf("\nThe  Sorted  array is : ");
+	print(a,n);
+	return 0;
+}
