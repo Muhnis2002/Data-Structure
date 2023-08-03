0aa8f1fd012644f21af62004a7fafca0fd52e8f6
muhanisss <muhammedmuhnis.cse21@jecc.ac.in>
2023-08-03 09:53:51 +0530

    Merge and Merge Sort Functions Completed

--- Merge Sort.c
+++ Merge Sort.c
@@ -36,3 +36,43 @@ int print(int a[],int l)
 	}
 	return 0;
 }
+int mergesort(int a[],int f,int l)
+{
+	int m;
+	if(f<l)
+	{
+		m=(f+l)/2;
+		mergesort(a,f,m);
+		mergesort(a,m+1,l);
+		merge(a,f,m,l);
+	}
+	return 0;
+}
+int merge(int a[],int f,int m,int l)
+{
+	int i,j,k,b[25];
+	for(i=f,j=m+1,k=f;i<=m&&j<=l;++k)
+	{
+		if(a[i]<=a[j])
+		{
+			b[k]=a[i++];
+		}
+		else
+		{
+			b[k]=a[j++];
+		}
+	}
+	while(i<=m)
+	{
+		b[k++]=a[i++];
+	}
+	while(j<=l)
+	{
+		b[k++]=a[j++];
+	}
+	for(k=f;k<=l;++k)
+	{
+		a[k]=b[k];
+	}
+	return 0;
+}
