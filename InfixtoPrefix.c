d85549ecbad955dfa1fa9eefb46f2ca747d5793f
mhnstech <muhammedmuhnis99@gmail.com>
2023-08-18 23:04:32 +0530

    Function to Convert an Infix to Postifix Expression and a Function to return the Precedence of a Operator are Added

--- InfixtoPrefix.c
+++ InfixtoPrefix.c
@@ -18,3 +18,60 @@ int main()
 	in_to_pr();
 	return 0;
 }
+int precedence(char c)
+{
+	switch(c)
+	{
+		case '+':
+		case '-': return 1;						
+		case '*':
+		case '/': return 2;
+		case '^': return 3;
+	}
+	return 0;
+}
+void in_to_pr()
+{
+	int i=strlen(in),j=0;
+	char x,y;
+	while(i>=0)
+	{
+		if((isalpha(in[i]))||(isdigit(in[i])))
+			p[j++]=in[i];
+		else if(in[i]==')')
+		    s[++top]=in[i];
+		else if(in[i]=='(')
+		{
+			while(s[top]!=')')
+			{
+				p[j++]=s[top--];
+			}
+			top--;
+		}
+		else
+		{ 
+			x=in[i],y=s[top];
+		    if((y==')')||(top==-1)||(precedence(y)<=precedence(x)))
+		    {
+				s[++top]=x;
+			}
+			else
+			{
+				while((top!=-1)&&(y!=')')&&(precedence(x)<precedence(y)))
+			    {
+				    p[j++]=s[top--];
+				    y=s[top];
+			    }
+			    s[++top]=x;
+			}
+		}
+		--i;
+	}
+	while(top!=-1)
+	{
+		p[j++]=s[top--];
+	}
+	p[j]='\0';
+	strrev(p);
+	printf("The Prefix Expression is : %s ",p);
+}
