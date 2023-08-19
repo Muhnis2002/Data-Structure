bc4780d6cf7ddd5cdddd788ef05e4bd2ca6c52e2
mhnstech <muhammedmuhnis99@gmail.com>
2023-08-19 23:22:35 +0530

    Read and Display Functions Added

--- PrefixEvaluvation.c
+++ PrefixEvaluvation.c
@@ -26,3 +26,41 @@ int main()
 	eval_pr();
 	return 0;
 }
+int read()
+{
+	printf("Enter the Prefix Expression Separating Each Token by Single Space : ");
+	gets(ch);
+	i=strlen(ch);
+	for(;i>=0;++k)
+	{
+		if(isdigit(ch[i]))
+		{
+			for(m=0;ch[i]!=' ';--i,++m)
+			{
+				po[k].ch[m]=ch[i];
+			}			
+			l=0;
+		}
+		else if(ch[i]=='*'||ch[i]=='+'||ch[i]=='-'||ch[i]=='/'||ch[i]=='^')
+		{
+			po[k].ch[0]=ch[i];
+			l=0;
+			--i;			
+		}
+		else 
+		{
+			++l;
+			--i;
+			--k;
+		}
+	}
+	return 0;
+}	
+int display()
+{
+	for(int j=k-1;j>=0;--j)
+	{
+		printf(" %s",po[j].ch);
+	}
+	return 0;
+}	
