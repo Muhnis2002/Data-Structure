9d6142550b11aaad371d3773897fac7c3bebb64c
mhnstech <muhammedmuhnis99@gmail.com>
2023-08-28 22:36:23 +0530

    Comments Added

--- Stack Using Linked List.c
+++ Stack Using Linked List.c
@@ -3,50 +3,63 @@
  C-Program to Implement Stack Using Linked List
  JEC21CS083
 */
+
 #include<stdio.h>
 #include<stdlib.h>
+
+// Define the structure for a node in the linked list
 struct node
 {
-	int dt;
-	struct node *ln;
+	int dt;              // Data of the node
+	struct node *ln;    // Pointer to the next node in the stack
 };
-struct node *top=NULL,*ptr,*new;
-int push(int it);
-int pop();
-int display();
+
+// Initialize the top of the stack as NULL
+struct node *top=NULL, *ptr, *new;
+
+// Function prototypes for stack operations
+int push(int it);       // Pushes an element onto the stack
+int pop();              // Pops an element from the stack
+int display();          // Displays the elements in the stack
+
 int main()
 {
 	int o;
+
 	printf("___Stack Operations___\n1.Push\n2.Pop\n3.Display\n4.Terminate Program");
 	op:
 	printf("\n\nEnter the Option Number : ");
 	scanf("%d",&o);
+
+	// Perform stack operation based on user's choice
 	if(o==1)
 	{
 		printf("Enter the Element to be Pushed : ");
 		scanf("%d",&o);
 		push(o);
-		goto op;
+		goto op;  // Go back to the main menu
 	}
 	else if(o==2)
 	{
 		pop();
-		goto op;
+		goto op;  // Go back to the main menu
 	}
 	else if(o==3)
 	{
 		display();
-		goto op;
+		goto op;  // Go back to the main menu
 	}
 	else if(o==4)
 		printf("Program Terminated");
 	else
 	{
 		printf("Wrong Input");
-		goto op;
+		goto op;  // Go back to the main menu
 	}
 	return 0;
 }
+
+// Function to push an element onto the stack
 int push(int it)
 {
 	new=(struct node*)malloc(sizeof(struct node));
@@ -56,6 +69,8 @@ int push(int it)
 	printf("%d is Pushed to the Stack Top",it);
 	return 0;	
 }
+
+// Function to pop an element from the stack
 int pop()
 {
 	if(top==NULL)
@@ -63,12 +78,14 @@ int pop()
 	else
 	{
 		ptr=top;
-		printf("%d is Poped from Stack",top->dt);
+		printf("%d is Popped from Stack",top->dt);
 		top=top->ln;
-		free(ptr);
+		free(ptr);  // Free the memory of the popped node
 	}
 	return 0;
 }
+
+// Function to display elements in the stack
 int display()
 {
 	if(top==NULL)
