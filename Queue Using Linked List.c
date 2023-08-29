cc9bed2346a0b96d192a98f78e28dd9de6ffd35b
mhnstech <muhammedmuhnis99@gmail.com>
2023-08-29 23:32:39 +0530

    Comments Added

--- Queue Using Linked List.c
+++ Queue Using Linked List.c
@@ -6,41 +6,49 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
+// Define the structure for each node in the linked list
 struct node
 {
-	int dt;
-	struct node *ln;
+	int dt;              // Data of the node
+	struct node *ln;    // Pointer to the next node
 };
-struct node *fr,*re,*new,*tem;
-int enqueue(int it);
-int dequeue();
-int display();
+
+// Global pointers for the front and rear of the queue
+struct node *fr, *re, *new, *tem;
+
+// Function prototypes
+int enqueue(int it);    // Function to enqueue an element
+int dequeue();          // Function to dequeue an element
+int display();          // Function to display the queue
+int main();             // Main function
+
 int main()
 {
 	int o;
 	printf("___QUEUE OPERATIONS___");
 	printf("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Terminate Program");
-	op:
+op:
 	printf("\nEnter the Option Number  : ");
-	scanf("%d",&o);
-	if(o==1)
+	scanf("%d", &o);
+	if (o == 1)
 	{
 		printf("Enter the Element to be Enqueued : ");
-		scanf("%d",&o);
+		scanf("%d", &o);
 		enqueue(o);
 		goto op;
 	}
-	else if(o==2)
+	else if (o == 2)
 	{
 		dequeue();
 		goto op;
 	}
-	else if(o==3)
+	else if (o == 3)
 	{
 		display();
 		goto op;
 	}
-	else if(o==4)
+	else if (o == 4)
 		printf("Program Terminated");
 	else
 	{
@@ -49,59 +57,65 @@ int main()
 	}
 	return 0;
 }
+
+// Function to enqueue an element
 int enqueue(int it)
 {
-	new=(struct node*)malloc(sizeof(struct node));
-	new->dt=it;
-	new->ln=NULL;
-	if(fr==NULL)
-		fr=re=new;
+	new = (struct node *)malloc(sizeof(struct node));
+	new->dt = it;
+	new->ln = NULL;
+	if (fr == NULL)
+		fr = re = new;
 	else
 	{
-		re->ln=new;
-		re=new;
+		re->ln = new;
+		re = new;
 	}
-	printf("%d is Enqueued\n",it);
+	printf("%d is Enqueued\n", it);
 	return 0;
 }
+
+// Function to dequeue an element
 int dequeue()
 {
-	if(fr==NULL)
+	if (fr == NULL)
 		printf("Queue is Empty\n");
-	else if(fr==re)
+	else if (fr == re)
 	{
-		tem=fr;
-		fr=re=NULL;
-		printf("%d is Dequeued\n",tem->dt);
+		tem = fr;
+		fr = re = NULL;
+		printf("%d is Dequeued\n", tem->dt);
 		free(tem);
 	}
 	else
 	{
-		tem=fr;
-		fr=fr->ln;
-		printf("%d is Dequeued\n",tem->dt);
+		tem = fr;
+		fr = fr->ln;
+		printf("%d is Dequeued\n", tem->dt);
 		free(tem);
 	}
 	return 0;
 }
+
+// Function to display the queue
 int display()
 {
-	if((fr==NULL)&&(re==NULL))
+	if ((fr == NULL) && (re == NULL))
 		printf("!!Queue is Empty!!\n");
 	else
 	{
-		for(tem=fr;tem->ln!=NULL;tem=tem->ln)
+		for (tem = fr; tem->ln != NULL; tem = tem->ln)
 			printf("______");
 		printf("______\n");
-		for(tem=fr;tem->ln!=NULL;tem=tem->ln)
+		for (tem = fr; tem->ln != NULL; tem = tem->ln)
 			printf("     |");
 		printf("     |\n");
-		for(tem=fr;tem->ln!=NULL;tem=tem->ln)
-			printf("  %d  |",tem->dt);
-		printf("  %d  |\n",tem->dt);
-		for(tem=fr;tem->ln!=NULL;tem=tem->ln)
-			printf("_____|");			
-		printf("_____|\n");	
+		for (tem = fr; tem->ln != NULL; tem = tem->ln)
+			printf("  %d  |", tem->dt);
+		printf("  %d  |\n", tem->dt);
+		for (tem = fr; tem->ln != NULL; tem = tem->ln)
+			printf("_____|");
+		printf("_____|\n");
 	}
 	return 0;
 }
